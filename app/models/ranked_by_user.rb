dc801d43739cd55b74fb5175434288010e9df470
Doug Youch <doug@cykod.com>
2011-02-21 16:08:56 -0500

    use amazon for lookup

--- app/models/ranked_by_user.rb
+++ app/models/ranked_by_user.rb
@@ -3,15 +3,12 @@
 class RankedByUser < DomainModel
   has_many :ranked_by_lists
 
-
   def lookup(value)
     self.search_amazon value
   end
 
   def lookup_by_identifier(identifier)
-       { :name => 'Item ' + rand(100).to_s,
-         :identifier => 'id2',
-        :description => 'Item description' + rand(100).to_s }
+    self.lookup_amazon identifier
   end
 
   def get_list(permalink)
@@ -26,14 +23,31 @@ class RankedByUser < DomainModel
     self.ranked_by_lists.create()
   end
 
-  def search_amazon(value)
-    self.amazon_product_web_service.item_search(value, 'All').collect do |item|
-      { :name => item['ItemAttributes']['Title'],
-        :link => item['ItemAttributes']['DetailPageURL'],
-        :description => item['ItemAttributes']['Title'],
-        :identifier => item['ItemAttributes']['DetailPageURL']
-      }
+  def parse_amazon_item(item)
+    reviews = item['EditorialReviews'] && item['EditorialReviews']['EditorialReview'] ? item['EditorialReviews']['EditorialReview'] : nil
+    reviews = reviews[0] if reviews.is_a?(Array)
+    description = reviews ? reviews['Content'] : nil
+
+    images = {}
+    [['SmallImage', :thumb], ['MediumImage', :preview], ['LargeImage', :full]].each do |image|
+      images[image[1]] = item[image[0]]['URL'] if item[image[0]]
     end
+
+    { :name => item['ItemAttributes']['Title'],
+      :link => item['DetailPageURL'],
+      :description => description,
+      :identifier => item['ASIN'],
+      :images => images
+    }
+  end
+
+  def search_amazon(value)
+    self.amazon_product_web_service.item_search(value, 'All').collect { |item| self.parse_amazon_item(item) }
+  end
+  
+  def lookup_amazon(value)
+    item = self.amazon_product_web_service.item_lookup(value)
+    self.parse_amazon_item item
   end
   
   def amazon_product_web_service
