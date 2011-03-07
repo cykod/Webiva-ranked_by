f086cd7e211fe505b3097a1f2ccbae9d48a2a911
Doug Youch <doug@cykod.com>
2011-03-07 11:35:52 -0500

    display lists items, edit items and lists

--- app/controllers/ranked_by/manage_controller.rb
+++ app/controllers/ranked_by/manage_controller.rb
@@ -3,14 +3,17 @@ class RankedBy::ManageController < ModuleController
   
   component_info 'RankedBy'
   
-  cms_admin_paths 'content'
+  cms_admin_paths 'content',
+                  'Ranked By Lists' => {:action => 'lists'}
   
   include ActiveTable::Controller
   active_table :list_table,
                 RankedByList,
-                [ :name,
+                [ :check,
+                  :name,
                   :description,
                   :author,
+                  :permalink,
                   hdr(:static, 'User'),
                   :views,
                   hdr(:static, '# Items'),
@@ -34,5 +37,66 @@ class RankedBy::ManageController < ModuleController
   def lists
     display_list_table(false)
     cms_page_path ['Content'], 'Ranked By Lists'
+  end
+  
+  def list
+    @list = RankedByList.find params[:path][0]
+    if request.post? && params[:list] && @list.update_attributes(params[:list])
+      flash[:notice] = 'List updated'
+      redirect_to :action => 'lists'
+      return
+    end
+    
+    cms_page_path ['Content', 'Ranked By Lists'], 'Edit List'
+  end
+  
+  active_table :list_item,
+                RankedByItem,
+                [ :check,
+                  hdr(:static, 'Image'),
+                  :name,
+                  :ranking,
+                  :description,
+                  :item_type,
+                  :identifier,
+                  :source_domain,
+                  :small_image_url,
+                  :large_image_url,
+                  :updated_at,
+                  :created_at
+                ]
+
+
+  def display_item_table(display=true)
+    @list ||= RankedByList.find params[:path][0]
+
+    active_table_action 'item' do |act, ids|
+      case act
+      when 'delete'
+        RankedByItem.destroy ids
+      end
+    end
+
+    @active_table_output = list_item_generate params, :conditions => ['ranked_by_list_id = ?', @list.id], :order => 'created_at DESC'
+
+    render :partial => 'item_table' if display
+  end
+  
+  def items
+    display_item_table(false)
+    cms_page_path ['Content', 'Ranked By Lists'], 'Items'
+  end
+  
+  def item
+    @list = RankedByList.find params[:path][0]
+    @item = @list.items.find params[:path][1]
+    
+    if request.post? && params[:item] && @item.update_attributes(params[:item])
+      flash[:notice] = 'Item updated'
+      redirect_to :action => 'items', :path => @list.id
+      return
+    end
+    
+    cms_page_path ['Content', 'Ranked By Lists', ['Items', {:action => 'items', :path => @list.id}]], "Edit #{@item.name}"
   end
 end
--- app/models/ranked_by_item.rb
+++ app/models/ranked_by_item.rb
@@ -3,7 +3,13 @@
 class RankedByItem < DomainModel
   belongs_to :ranked_by_list
 
+  has_domain_file :image_file_id
 
+  def small_image_name
+    self.small_image_url.split('/')[-1] if self.small_image_url
+  end
 
-
+  def large_image_name
+    self.large_image_url.split('/')[-1] if self.large_image_url
+  end
 end
